4059f313c164087e34951eec43c264974329aa19
Wei9023 <vv880923@gmail.com>
2019-10-25 22:06:25 -0700

    update images

--- src/js/index.js
+++ src/js/index.js
@@ -2,14 +2,17 @@
 //https://www.food2fork.com/api/search?key=YOUR_API_KEY&q=chicken%20breast&page=2
 
 import Search from './modules/Search';
-
+import * as searchView from './views/searchView';
+import { elements } from './views/base'
 //global state  of the app
 
 const state = {}
 
 const constrolSearch = async () =>{
     //1) get query from view
-    const query ='pizza' //TODO
+    const query = searchView.getInput(); //TODO
+    // const query = 'pizza'
+    // console.log(query);
 
     if(query) {
         // 2) new search object and add to state
@@ -21,12 +24,13 @@ const constrolSearch = async () =>{
         await state.search.getResults();
 
         //5) render results on UI
-        console.log(state.search.result);
+        // console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 }
-document.querySelector('.search').addEventListener('submit', e =>{
+elements.searchForm.addEventListener('submit', e =>{
     e.preventDefault();
     constrolSearch();
-})
+});
 
 // search.getResults();
